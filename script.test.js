12a34b9034e44f4938c4a413ce18e63196d9478b
jackbash1 <145982675+jackbash1@users.noreply.github.com>
2024-12-17 13:21:21 +0000

    Update script.test.js

--- script.test.js
+++ script.test.js
@@ -8,5 +8,5 @@ const { convertToInHg, convertToHpa } = require('./script')
 test('that the user input is not a string', () => {
     expect(typeof convertToInHg(1013)).toBe('string');
     expect(typeof convertToInHg('one zero one three')).not.toBe('string');
-    expect(() => convertToInHg('abc')).toThrow("Input must be a number.");
+    expect(() => convertToInHg('abc')).toThrow("Input must be a number");
 });
