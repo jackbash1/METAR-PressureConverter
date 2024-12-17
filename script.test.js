a1fafa3199cba415aa641f41ca724191d4ef6e3f
jackbash1 <145982675+jackbash1@users.noreply.github.com>
2024-12-17 13:12:29 +0000

    Update script.test.js

--- script.test.js
+++ script.test.js
@@ -3,7 +3,7 @@ test ('that Jest is working', () => {
     expect(2+2).toBe(4);
 }) 
 
-const { convertToInHg, convertToHpa } = require('../script')
+const { convertToInHg, convertToHpa } = require('./script')
 
 test('that the user input is a number and advises when not', () => {
     expect(typeof convertToInHg(1013)).toBe('number');
