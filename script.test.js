a97ace27bf4d91505fbe1cfe0e52adcf3e1863ff
jackbash1 <145982675+jackbash1@users.noreply.github.com>
2024-12-17 12:55:39 +0000

    adjusting the unit tests

--- script.test.js
+++ script.test.js
@@ -1,8 +1,9 @@
-// test ('that Jest is working', () => {
-//     expect(2+2).toBe(4);
-// }) 
+// testing that jest is working initially
+test ('that Jest is working', () => {
+    expect(2+2).toBe(4);
+}) 
 
-const { input } = require('./script.js')
+const { convertToHg, convertToHpa } = require('../script')
 
 test('that the user input is a number', () => {
     expect(typeof input(1013)).toBe('number');
