0197c4cfd4ce4df47cd10a0a3e3490e09e2b9150
jackbash1 <jb220056a@nulondon.ac.uk>
2024-12-18 00:06:39 +0000

    adding more unit tests

--- script.test.js
+++ script.test.js
@@ -6,7 +6,7 @@ test ('that Jest is working', () => {
 const { convertToInHg, convertToHpa } = require('./script')
 
 describe('convertToInHg function tests', () => {
-    test('that the user input is not a string', () => {
+    test('that the user input is a string', () => {
         expect(typeof convertToInHg(1013)).toBe('string');
     });
     
@@ -14,5 +14,25 @@ describe('convertToInHg function tests', () => {
         expect(() => {
             convertToInHg('abc');
         }).toThrow("Input must be a number");
+    });
+
+    test('tests that 1013 hPa is 29.91 inHg', () => {
+        expect(convertToInHg(1013)).toBe("29.91")
+    });
+});
+
+describe('convertToHPa function tests', () => {
+    test('that the user input is a string', () => {
+        expect(typeof convertToHpa(29.92)).toBe('string');
+    });
+    
+    test('throws error for non-numeric input', () => {
+        expect(() => {
+            convertToHpa('xyz');
+        }).toThrow("Input must be a number");
+    });
+
+    test('tests that 29.92 inHg is 1013.21 hPa', () => {
+        expect(convertToHpa(29.92)).toBe("1013.21")
     });
 });
