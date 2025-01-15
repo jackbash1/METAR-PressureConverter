0c703e7d673a9e91458422d9df5947e019da5635
jackbash1 <145982675+jackbash1@users.noreply.github.com>
2025-01-15 23:15:10 +0000

    Update script.js with new function code

--- script.js
+++ script.js
@@ -10,7 +10,22 @@ function convertToHpa(inHg) {
     return (parseFloat(inHg) * 33.8639).toFixed(2);
 }
 
+function convertQNH(qnhType, qnhValueRaw) {
+    if (qnhType === 'A') {
+        // convert inHg value to the correct decimal format
+        qnhValue = (parseFloat(qnhValueRaw) / 100).toFixed(2) // fixes the value to 2 d.p as required
+        return qnhValue;
+    }
+    else if (qnhType === 'Q') {
+        // keeps the hPa value the same as reads correct in METAR
+        qnhValue = parseInt(qnhValueRaw, 10)
+        return qnhValue;
+    }    
+    else {
+        throw new Error('No QNH found in the METAR!')
+}}   
+
 // exports for the testing functions
 if (typeof module !== "undefined") {
-    module.exports = { convertToInHg, convertToHpa };
+    module.exports = { convertToInHg, convertToHpa, convertQNH };
 }
